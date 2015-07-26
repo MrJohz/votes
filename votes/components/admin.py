b676aae0479632c7819a3da70060704b16b5bc28
Jonathan <jonathan.frere@gmail.com>
2015-07-26 23:54:44 +0100

    Allow modification of questions via admin interface

--- votes/components/admin.py
+++ votes/components/admin.py
@@ -100,13 +100,63 @@ class QuestionInterface(base.RestfulComponent):
                              question=get_model_instance(question_id, models.Question))
 
     def do_put(self, question_id, form):
-        from pprint import pprint
-        pprint(form)
-        return ''
+        question=get_model_instance(question_id, models.Question)
+        question.text = form.pop('text', question.text)
+        question.description = form.pop('desc', question.description)
+        question.save()
+
+        saved_ids = []
+        for key in form:
+            if key.startswith('answer-') and key.endswith('-text') and form[key]:
+                key_id = key[7:-5]
+                try:
+                    key_id = int(key_id)
+                except ValueError:
+                    answer = models.Answer(question=question, text=form[key])
+                    answer.save()
+                    for i in map(int, destr(form.get('answer-%s-sys' % key_id))):
+                        try:
+                            system = models.System.get(id=i)
+                        except models.System.DoesNotExist:
+                            continue
+                        answer.systems.add(system)
+                    answer.save()
+                    saved_ids.append(answer.id)
+                else:
+                    try:
+                        answer = models.Answer.get(
+                            (models.Answer.id == key_id) &
+                            (models.Answer.question == question))
+                    except models.Answer.DoesNotExist:
+                        continue
+                    answer.text = form[key]
+                    answer.save()
+                    answer.systems.clear()
+                    for i in map(int, destr(form.get('answer-%s-sys' % key_id))):
+                        try:
+                            system = models.System.get(id=i)
+                        except models.System.DoesNotExist:
+                            continue
+                        if system not in answer.systems:
+                            answer.systems.add(system)
+                    answer.save()
+                    saved_ids.append(key_id)
+        models.Answer.delete().where(
+            ~(models.Answer.id << saved_ids) &
+            (models.Answer.question == question)).execute()
+
+        raise cherrypy.HTTPRedirect('/admin/questions/' + str(question_id) + '/put')
 
     def delete(self, question_id):
         return self.template('admin/delete_question.html',
                              question=get_model_instance(question_id, models.Question))
+
+    def do_delete(self, question_id, form):
+        if form.get('delete', '').lower() != 'yes':
+            raise cherrypy.HTTPRedirect('/admin/questions/')
+
+        get_model_instance(question_id, models.Question).delete_instance(recursive=True)
+        raise cherrypy.HTTPRedirect('/admin/questions/')
 
 
 class AdminInterface(base.AuthenticatedComponent):
